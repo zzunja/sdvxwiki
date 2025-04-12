1dafaba1bda3fa4fdc584d545a42fd2667fdfb69
zzunja <mason.mccombs@my.utsa.edu>
2025-04-12 15:28:27 -0500

    split of song select and options

--- content/startingout/_meta.js
+++ content/startingout/_meta.js
@@ -1,12 +1,14 @@
 export default{
+  songselect: {
+    title: 'Song Select',
+    theme: { timestamp: false },
+  },
   options: {
-    title: 'Song Select/Options',
+    title: 'Options Menu',
     theme: { timestamp: false },
   },
   playingscoring: {
     title: 'Playing and understanding your score',
     theme: { timestamp: false },
   },
-  
-  
 }
