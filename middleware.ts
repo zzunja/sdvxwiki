cda7f8b644d74136678851181b62b230e2c578b9
zzunja <mason.mccombs@my.utsa.edu>
2025-04-18 14:15:00 -0500

    stop robots from pinging /wp-admin

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,21 @@
-export {middleware} from 'nextra/locales'
+import { NextResponse } from 'next/server'
+import { NotFoundPage } from 'nextra-theme-docs';
+import {middleware as middlewareNextra} from 'nextra/locales'
 
+
+export function middleware(request){
+  const url = request.nextUrl.pathname
+
+  if(url.includes('/wp-admin') || url.includes('/wordpress')){
+    //prevent the auto robots that just go to /wp-admin and /wordpress on repeat
+    return new NextResponse('Not Found', { status: 404 });
+  } 
+
+  return middlewareNextra(request)
+}
 export const config = {
   //this is for exlcuding what gets redirected with language. if you have anything to add, add it here. ".*\\..*" is for the whole public folder
   matcher: [
-    '/((?!api|public/|_next|_pagefind|favicon.ico|sitemap.xml|.*\\..*).*)',
+    '/((?!api|public/|_next|_pagefind|favicon.ico|sitemap.xml|.*\\..*).*)', '/wp-admin/:path*','/wordpress/:path*'
   ]
 }
