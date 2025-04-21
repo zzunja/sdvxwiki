cdf2ad3c89e78ec8e8a6e063e8be76907e0f7625
Hoshikara <anguyen1905@gmail.com>
2025-04-21 01:52:02 -0700

    add laser simplifications guide

--- content/en/guides/_meta.js
+++ content/en/guides/_meta.js
@@ -3,12 +3,16 @@ export default {
     title: 'Hand Positioning',
     theme: { timestamp: false },
   },
-  'pattern-simplification': {
-    title: 'Pattern Simplification and Cheese',
-    theme: { timestamp: false },
-  },
   'ksm-controller-setup': {
     title: 'KSM Controller Setup',
+    theme: { timestamp: false },
+  },
+  'laser-simplifications': {
+    title: 'Laser Simplifications',
+    theme: { timestamp: false },
+  },
+  'pattern-simplification': {
+    title: 'Pattern Simplification and Cheese',
     theme: { timestamp: false },
   },
 }
