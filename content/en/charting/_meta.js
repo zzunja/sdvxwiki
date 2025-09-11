c38f542eb66de778ee3ee0531bd71a5e183d9970
zzunja <mason.mccombs@my.utsa.edu>
2025-09-11 00:22:54 -0500

    sync done

--- content/en/charting/_meta.js
+++ content/en/charting/_meta.js
@@ -1,5 +1,9 @@
 export default {
-  'kshfile': {
+  guide: {
+    title: 'Charting Guide',
+    theme: { timestamp: false, toc: false},
+  },
+  kshfile: {
     title: '.ksh file format',
     theme: { timestamp: false, toc: false},
   },
